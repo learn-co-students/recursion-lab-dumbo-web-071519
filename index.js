e88d4c735d0258cb690590cc0e7082c7316be756
Ross Perkel <rossperkel@gmail.com>
2019-11-19 14:26:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,52 @@
+import { stringify } from "querystring";
+import { reverse } from "dns";
+
 // Code your solution here!
 
+function printString(myString) {
+    console.log(myString[0]);
+
+    if (myString.length > 1) {
+        let mySubString = myString.substring(1, myString.length);
+        printString(mySubString);
+    } else {
+        return true;
+    }
+}
+function reverseString(str) {
+    return (str === '') ? '' : reverseString(str.substr(1)) + str.charAt(0);
+}
+
+function isPalindrome(string){
+    if(string.length < 2){
+        return true
+    } else if(string[0] === string[string.length - 1]){
+        return isPalindrome(string.substring(1, string.length -1))
+    } else{
+        return false
+    }
+}
+
+function addUpTo(array, index){
+    return index ? array[index] + addUpTo(array, --index)  : array[index]
+}
+// addUpTo([1,2,3,4,5], 3) // 10
+
+function maxOf(array){
+    if(array.length === 1){
+        return array[0]
+    } else{
+        return Math.max(array.pop(), maxOf(array))
+    }
+}
+
+function includesNumber(array, element){
+    if(!array.length){
+        return false
+    }
+    if(array[0] === element){
+        return true
+    } else{
+        return includesNumber(array.slice(1), element)
+    }
+}
